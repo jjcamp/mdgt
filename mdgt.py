38c15820004d726698cb0c11ba6045d823ab2bb3
John <jjcamp@gmail.com>
2016-02-16 20:59:27 -0500

    Fixed --modules command to not require the positional arguments.

--- mdgt.py
+++ mdgt.py
@@ -81,23 +81,25 @@ if __name__ == "__main__":
 	import argparse
 	parser = argparse.ArgumentParser()
 	# Required arguments
-	parser.add_argument('module',
+	parser.add_argument('module', nargs = '?',
 	               help = "Which module to call (or, the type of object to query).")
-	parser.add_argument('query',
+	parser.add_argument('query', nargs = '?',
 	               help = "The query for the module to consume.")
+	# Other options
+	parser.add_argument('-m', '--modules', action = 'store_true',
+	               help = "List available modules and exit.")
 	# These arguments affect the output and are exclusive
 	outputGroup = parser.add_mutually_exclusive_group()
 	outputGroup.add_argument('-c', '--console', action = 'store_true',
 	                  help = "Output console-formatted text (default).")
 	outputGroup.add_argument('-j', '--json', action = 'store_true',
 	                  help = "Output json.")
-	# Other options
-	parser.add_argument('-m', '--modules', action = 'store_true',
-	               help = "List available modules and exit.")
 	args = parser.parse_args()
 
 	if args.modules:
 		listMods()
+	elif (not args.module) and (not args.query):
+		print("Module and query required. See --help")
 	elif args.json:
 		jsonPrint(scrape(loadMod(args.module), args.query))
 	else:
