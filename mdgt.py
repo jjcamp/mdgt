3a69450b0d83264fdbc3fbec1601ff96b9860617
John <jjcamp@gmail.com>
2016-02-17 19:28:57 -0500

    Refactored module functions their own python module/class.

--- mdgt.py
+++ mdgt.py
@@ -1,57 +1,6 @@
-from lxml import html
 from pathlib import Path
-import requests
 import json
-
-def loadMod(name):
-	p = Path("./mod/" + name + ".json")
-	if p.exists() == False:
-		raise RuntimeError("Module " + name + " does not exist.")
-	with p.open() as f:
-		return json.loads(f.read())
-
-def scrape(mod, query):
-	# Get search information
-	search = mod['search']
-	uriSearch = search['uriRoot'] + search['searchPath'].replace('*', query)
-	page = requests.get(uriSearch)
-	tree = html.fromstring(page.content)
-	# Some sources may have a feeling lucky option, so this part is optional
-	if 'firstResultXpath' in search:
-		resNode = tree.xpath(search['firstResultXpath'])[0]
-		resUri = resNode.get('href')
-		# Check for a relative path
-		if resUri[0] == '/':
-			resUri = search['uriRoot'] + resUri
-		page = requests.get(resUri)
-		tree = html.fromstring(page.content)
-
-	# Start with the MD object's root and recurse through the tree
-	data = dict()
-	parseNode(tree, mod['root'], data)
-	return data
-	#print(json.dumps(data))
-
-def parseNode(parentTree, modNode, dataDict):
-	dataList = []
-	tree = parentTree.xpath(modNode['xpath'])
-	# If repeat is not present or set to true, then just use the first result
-	if 'repeat' not in modNode or modNode['repeat'] == False:
-		tree = tree[0:1]
-	for t in tree:
-		if 'value' in modNode:
-			val = modNode['value']
-			if val['type'] == 'text':
-				dataList.append(t.text.strip())
-			elif val['type'] == 'attr':
-				dataList.append(t.get(val['attr']))
-			if len(dataList) == 1:
-				dataDict[val['name']] = dataList[0]
-			elif len(dataList) > 1:
-				dataDict[val['name']] = dataList
-		if 'items' in modNode:
-			for i in modNode['items']:
-				parseNode(t, i, dataDict)
+from module import module
 
 def jsonPrint(dataDict):
 	print(json.dumps(dataDict))
@@ -101,6 +50,8 @@ if __name__ == "__main__":
 	elif (not args.module) and (not args.query):
 		print("Module and query required. See --help")
 	elif args.json:
-		jsonPrint(scrape(loadMod(args.module), args.query))
+		mod = module(args.module)
+		jsonPrint(mod.scrape(args.query))
 	else:
-		consolePrint(scrape(loadMod(args.module), args.query))
+		mod = module(args.module)
+		consolePrint(mod.scrape(args.query))
