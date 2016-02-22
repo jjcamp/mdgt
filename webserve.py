2cf9b3024cd9fb5c754a77183349202554c3b83a
John <jjcamp@gmail.com>
2016-02-22 10:29:42 -0500

    Added docstrings to webserve.py

--- webserve.py
+++ webserve.py
@@ -1,13 +1,20 @@
+'''webserve.py
+mdgt web server module.
+'''
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from pathlib import Path
 from provider import Provider
 import json
 # Future ssl use, leaving it here as a reminder
-#import ssl
+# import ssl
+
 
 class mdgtHandler(BaseHTTPRequestHandler):
-    
+    '''HTTP Request Handler
+    Extends BaseHTTPRequestHandler in order to process HTTP GET requests.
+    '''
     def do_GET(self):
+        '''Processes HTTP GET requests'''
         args = self.path.split('/', 3)
         if len(args) < 2:
             self.JSON_error("Not enough parameters.")
@@ -25,9 +32,9 @@ class mdgtHandler(BaseHTTPRequestHandler):
             self.wfile.write(json.dumps(prov.scrape(args[2])).encode('utf-8'))
         else:
             self.JSON_error("Invalid arguments.")
-        return
-        
+
     def list_providers(self):
+        '''Outputs a list of providers.'''
         p = Path('providers')
         provFiles = list(p.glob('*.json'))
         provs = [pf.stem for pf in provFiles]
@@ -35,18 +42,27 @@ class mdgtHandler(BaseHTTPRequestHandler):
         self.send_header("Content-type", "application/json")
         self.end_headers()
         self.wfile.write(json.dumps(provs).encode('utf-8'))
-        return
-        
+
     def JSON_error(self, message):
+        '''Outputs an error message.
+
+        Args:
+            message (str): A useful error message.
+        '''
         # Might want a different response
         self.send_response(200)
         self.send_header("Content-type", "application/json")
         self.end_headers()
         self.wfile.write(json.dumps({"error_message": message}).encode('utf-8'))
-        return
-        
+
 
 def serve(port=8181):
+    '''Starts a web server service.
+
+    Args:
+        port (Optional[int]): The port the server should listen at.  Defaults
+            to 8181.
+    '''
     address = ('', port)
     httpd = HTTPServer(address, mdgtHandler)
     try:
