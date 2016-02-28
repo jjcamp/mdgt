05e8d9e8fdbb8888132d74dfd5c04ea01a8116b6
John <jjcamp@gmail.com>
2016-02-28 02:21:33 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mdgt",
+    version="0.1.0",
+    packages=find_packages(),
+    author="John Camp and Contributors",
+    description="Microdata-Parsing Microservice",
+    url="https://github.com/jjcamp/mdgt",
+    keywords="microdata"
+    long_description=open('README.md').read(),
+    license="MIT",
+    install_requires=[
+        "lxml >= 3.5.0",
+        "requests >= 2.9.1"
+    ]
+)
